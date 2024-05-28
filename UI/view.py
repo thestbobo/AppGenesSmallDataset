aa0ed0ece7d0c3a872d3ef8d2b190c8a3bcc225f
FranPi14 <123479714+FranPi14@users.noreply.github.com>
2024-05-28 18:53:17 +0200

    Update view.py
    update view

--- UI/view.py
+++ UI/view.py
@@ -8,32 +8,31 @@ class View(ft.UserControl):
         self._page = page
         self._page.title = "Template application using MVC and DAO"
         self._page.horizontal_alignment = 'CENTER'
-        self._page.theme_mode = ft.ThemeMode.DARK
+        self._page.theme_mode = ft.ThemeMode.LIGHT
         # controller (it is not initialized. Must be initialized in the main, after the controller is created)
         self._controller = None
+
         # graphical elements
         self._title = None
         self.txt_name = None
-        self.btn_hello = None
+
+        self.btn_graph = None
+        self.btn_countedges = None
+        self.btn_search = None
+
         self.txt_result = None
+        self.txt_result2 = None
+        self.txt_result3 = None
+
         self.txt_container = None
 
     def load_interface(self):
         # title
-        self._title = ft.Text("Hello World", color="blue", size=24)
+        self._title = ft.Text("Lab14-Simulazione esame gene_small", color="blue", size=24)
         self._page.controls.append(self._title)
 
-        #ROW with some controls
-        # text field for the name
-        self.txt_name = ft.TextField(
-            label="name",
-            width=200,
-            hint_text="Insert a your name"
-        )
-
-        # button for the "hello" reply
-        self.btn_hello = ft.ElevatedButton(text="Hello", on_click=self._controller.handle_hello)
-        row1 = ft.Row([self.txt_name, self.btn_hello],
+        self.btn_graph = ft.ElevatedButton(text="Crea Grafo", on_click=self._controller.handle_graph)
+        row1 = ft.Row([self.btn_graph],
                       alignment=ft.MainAxisAlignment.CENTER)
         self._page.controls.append(row1)
 
@@ -42,6 +41,32 @@ class View(ft.UserControl):
         self._page.controls.append(self.txt_result)
         self._page.update()
 
+        #ROW with some controls
+        # text field for the name
+        self.txt_name = ft.TextField(
+            label="soglia",
+            width=200,
+            hint_text="Soglia (s)"
+        )
+        self.btn_countedges = ft.ElevatedButton(text="Conta Archi", on_click=self._controller.handle_countedges)
+        row2 = ft.Row([self.txt_name, self.btn_countedges],
+                      alignment=ft.MainAxisAlignment.CENTER)
+        self._page.controls.append(row2)
+
+        # List View where the reply is printed
+        self.txt_result2 = ft.ListView(expand=1, spacing=10, padding=20, auto_scroll=True)
+        self._page.controls.append(self.txt_result2)
+        self._page.update()
+
+        self.btn_search = ft.ElevatedButton(text="Cerca cammino", on_click=self._controller.handle_search)
+        row3 = ft.Row([self.btn_search],
+                      alignment=ft.MainAxisAlignment.CENTER)
+        self._page.controls.append(row3)
+
+        # List View where the reply is printed
+        self.txt_result3 = ft.ListView(expand=1, spacing=10, padding=20, auto_scroll=True)
+        self._page.controls.append(self.txt_result3)
+        self._page.update()
     @property
     def controller(self):
         return self._controller
